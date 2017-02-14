42da9b79855f8f50f95c4df9f5b32d9fa0ba6332
hasnainjafri <hasnainjafri@WA256.local>
2017-02-14 15:22:34 +0500

    Add More Focus and better camera Exposure

--- BarcodeScanner/ScanController.m
+++ BarcodeScanner/ScanController.m
@@ -9,6 +9,7 @@
 #import "ScanController.h"
 #import <AVFoundation/AVFoundation.h>
 #import "LicenseParser.h"
+#import "CameraFocusSquare.h"
 
 @interface ScanController () <AVCaptureMetadataOutputObjectsDelegate>
 {
@@ -19,6 +20,7 @@
 @property (nonatomic, strong) AVCaptureSession *captureSession;
 @property (nonatomic, strong) AVCaptureVideoPreviewLayer *captureLayer;
 @property (nonatomic, strong) AVCaptureMetadataOutput *captureMetadataOutput;
+@property (nonatomic, strong) AVCaptureDevice *captureDevice;
 
 
 @end
@@ -42,7 +44,7 @@
     
     if(isLayout == FALSE){
         isLayout = TRUE;
-    [self setupScanningSession];
+        [self setupScanningSession];
     }
 }
 
@@ -80,7 +82,7 @@
     
     NSError *error;
     // Set camera capture device to default and the media type to video.
-    AVCaptureDevice *captureDevice = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+    _captureDevice = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
     
     NSArray *videoDevices = [AVCaptureDevice devicesWithMediaType:AVMediaTypeVideo];
     
@@ -88,20 +90,30 @@
         
         if (device.position == AVCaptureDevicePositionBack){
             
-            captureDevice = device;
+            _captureDevice = device;
             break;
         }
     }
     
-    if ([captureDevice isFocusModeSupported:AVCaptureFocusModeContinuousAutoFocus]) {
+    if ([_captureDevice isFocusModeSupported:AVCaptureFocusModeAutoFocus]) {
     }
-
-    if([captureDevice isLowLightBoostSupported]){
-        [captureDevice setAutomaticallyEnablesLowLightBoostWhenAvailable:YES];
+    
+    if([_captureDevice isExposureModeSupported:AVCaptureExposureModeContinuousAutoExposure])
+    {
+        if([_captureDevice lockForConfiguration:nil]) {
+            
+            [_captureDevice setExposureMode:AVCaptureExposureModeContinuousAutoExposure];
+        }
+        [_captureDevice unlockForConfiguration];
+        
+    }
+    
+    if([_captureDevice isLowLightBoostSupported]){
+        [_captureDevice setAutomaticallyEnablesLowLightBoostWhenAvailable:YES];
     }
     
     // Set video capture input: If there a problem initialising the camera, it will give am error.
-    AVCaptureDeviceInput *input = [AVCaptureDeviceInput deviceInputWithDevice:captureDevice error:&error];
+    AVCaptureDeviceInput *input = [AVCaptureDeviceInput deviceInputWithDevice:_captureDevice error:&error];
     
     if (!input) {
         NSLog(@"Error Getting Camera Input");
@@ -109,7 +121,7 @@
     }
     // Adding input souce for capture session. i.e., Camera
     [self.captureSession addInput:input];
-
+    
     _captureMetadataOutput = [[AVCaptureMetadataOutput alloc] init];
     
     // Set output to capture session. Initalising an output object we will use later.
@@ -117,13 +129,13 @@
     
     //_captureMetadataOutput.rectOfInterest = CGRectMake(0, 0, 1, 1);
     
-   /* CGRect scanRect = CGRectMake(0, 50, self.view.frame.size.width, 200);
-    float x = scanRect.origin.x/1334;
-    float y = scanRect.origin.y/750;
-    float width = scanRect.size.width/1334;
-    float height = scanRect.size.height/750;
-    CGRect scanRectTransformed = CGRectMake(x, y, width, height);
-    _captureMetadataOutput.rectOfInterest = scanRectTransformed;*/
+    /* CGRect scanRect = CGRectMake(0, 50, self.view.frame.size.width, 200);
+     float x = scanRect.origin.x/1334;
+     float y = scanRect.origin.y/750;
+     float width = scanRect.size.width/1334;
+     float height = scanRect.size.height/750;
+     CGRect scanRectTransformed = CGRectMake(x, y, width, height);
+     _captureMetadataOutput.rectOfInterest = scanRectTransformed;*/
     
     
     
@@ -133,7 +145,7 @@
     dispatch_queue_t dispatchQueue;
     dispatchQueue = dispatch_queue_create("scanQueue", NULL);
     [_captureMetadataOutput setMetadataObjectsDelegate:self queue:dispatchQueue];
-
+    
     // Delegate should implement captureOutput:didOutputMetadataObjects:fromConnection: to get callbacks on detected metadata.
     [_captureMetadataOutput setMetadataObjectTypes:@[AVMetadataObjectTypePDF417Code]];
     //captureMetadataOutput.rectOfInterest = self.cameraPreviewView.layer.bounds;
@@ -143,18 +155,18 @@
     [self.captureLayer setVideoGravity:AVLayerVideoGravityResizeAspectFill];
     [self.captureLayer setFrame:self.cameraPreviewView.layer.bounds];
     //[self.captureLayer rectForMetadataOutputRectOfInterest:self.captureLayer.bounds];
-
+    
     // Adding the camera AVCaptureVideoPreviewLayer to our view's layer.
     [self.cameraPreviewView.layer addSublayer:self.captureLayer];
     
     
-   /*UIView *highlightView = [[UIView alloc] init];
-    highlightView.autoresizingMask = UIViewAutoresizingFlexibleTopMargin|UIViewAutoresizingFlexibleLeftMargin|UIViewAutoresizingFlexibleRightMargin|UIViewAutoresizingFlexibleBottomMargin;
-    highlightView.layer.borderColor = [UIColor greenColor].CGColor;
-    highlightView.layer.borderWidth = 2;
-    [highlightView setFrame:CGRectMake(5, self.view.center.y-50, self.view.frame.size.width-10, 100)];
-    //[highlightView setFrame:scanRect];
-    [self.cameraPreviewView addSubview:highlightView];*/
+    /*UIView *highlightView = [[UIView alloc] init];
+     highlightView.autoresizingMask = UIViewAutoresizingFlexibleTopMargin|UIViewAutoresizingFlexibleLeftMargin|UIViewAutoresizingFlexibleRightMargin|UIViewAutoresizingFlexibleBottomMargin;
+     highlightView.layer.borderColor = [UIColor greenColor].CGColor;
+     highlightView.layer.borderWidth = 2;
+     [highlightView setFrame:CGRectMake(5, self.view.center.y-50, self.view.frame.size.width-10, 100)];
+     //[highlightView setFrame:scanRect];
+     [self.cameraPreviewView addSubview:highlightView];*/
     
     [self.captureSession startRunning];
 }
@@ -194,13 +206,90 @@
                 dispatch_sync(dispatch_get_main_queue(), ^{
                     [self.captureSession stopRunning];
                     self.scannedBarcode.text = capturedBarcode;
-
+                    
                     [self parseLicenseData:capturedBarcode];
                 });
                 return;
             }
         }
     }
+}
+
+- (void) tapSent:(CGPoint)sender {
+    
+    
+    CGPoint aPoint = sender;//[sender locationInView:_imageStreamV];
+    if (_captureDevice != nil) {
+        if([_captureDevice isFocusPointOfInterestSupported] &&
+           [_captureDevice isFocusModeSupported:AVCaptureFocusModeAutoFocus]) {
+            
+            // we subtract the point from the width to inverse the focal point
+            // focus points of interest represents a CGPoint where
+            // {0,0} corresponds to the top left of the picture area, and
+            // {1,1} corresponds to the bottom right in landscape mode with the home button on the right—
+            // THIS APPLIES EVEN IF THE DEVICE IS IN PORTRAIT MODE
+            // (from docs)
+            // this is all a touch wonky
+            double pX = aPoint.x / self.view.bounds.size.width;
+            double pY = aPoint.y / self.view.bounds.size.height;
+            double focusX = pY;
+            // x is equal to y but y is equal to inverse x ?
+            double focusY = 1 - pX;
+            
+            //NSLog(@"SC: about to focus at x: %f, y: %f", focusX, focusY);
+            if([_captureDevice isFocusPointOfInterestSupported] && [_captureDevice isFocusModeSupported:AVCaptureFocusModeAutoFocus]) {
+                
+                if([_captureDevice lockForConfiguration:nil]) {
+                    [_captureDevice setFocusPointOfInterest:CGPointMake(focusX, focusY)];
+                    [_captureDevice setFocusMode:AVCaptureFocusModeAutoFocus];
+                    [_captureDevice setExposurePointOfInterest:CGPointMake(focusX, focusY)];
+                    [_captureDevice setExposureMode:AVCaptureExposureModeContinuousAutoExposure];
+                    
+                    
+                    //NSLog(@"SC: Done Focusing");
+                }
+                [_captureDevice unlockForConfiguration];
+            }
+        }
+    }
+    
+}
+
+- (void) touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    UITouch *touch = [[event allTouches] anyObject];
+    CGPoint touchPoint = [touch locationInView:touch.view];
+    [self tapSent:touchPoint];
+    
+    CameraFocusSquare *oldCamFocus = (CameraFocusSquare*)[self.view viewWithTag:1010];
+    
+    if(oldCamFocus != nil){
+        [oldCamFocus.layer removeAllAnimations];
+        [oldCamFocus removeFromSuperview];
+        oldCamFocus = nil;
+    }
+    
+    CameraFocusSquare *camFocus = [[CameraFocusSquare alloc]initWithFrame:CGRectMake(touchPoint.x-40, touchPoint.y-40, 80, 80)];
+    [camFocus setTag:1010];
+    [camFocus setBackgroundColor:[UIColor clearColor]];
+    [self.view addSubview:camFocus];
+    [camFocus setNeedsDisplay];
+    
+    SEL animeEnd = NSSelectorFromString(@"animationDidEnd");
+    
+    [UIView beginAnimations:nil context:NULL];
+    [UIView setAnimationDuration:3.0];
+    [UIView setAnimationDidStopSelector:animeEnd];
+    [camFocus setAlpha:0.0];
+    [UIView commitAnimations];
+}
+
+-(void)animationDidEnd{
+    NSLog(@"");
+    
+    CameraFocusSquare *camFocus = (CameraFocusSquare*)[self.view viewWithTag:1010];
+    [camFocus removeFromSuperview];
+    camFocus = nil;
 }
 
 -(void)parseLicenseData:(NSString*)message
